9f9d721193a60180da6264af003431749edcae14
ArilNarindra <95149249+ArilNarindra@users.noreply.github.com>
2023-01-13 21:41:41 +0700

    Update stopjadibot.js

--- stopjadibot.js
+++ stopjadibot.js
@@ -1,1 +1,10 @@
-
+let stopjadibot = async (m, { conn, isOwner }) => {
+  if (global.conn.user.jid == conn.user.jid || !isOwner) throw 'nothing to do here.'
+  else {
+    await conn.reply(m.chat, 'successfully disconnected', m)
+    conn.close()
+  }
+}
+stopjadibot.help = ['stop']
+stopjadibot.tags = ['jadibot']
+stopjadibot.command = /^(stop)$/i
