19843ce1178ab569f0c2bb3641fc02ef2629ecf1
ArilNarindra <95149249+ArilNarindra@users.noreply.github.com>
2023-01-13 21:40:07 +0700

    Update getcode.js

--- getcode.js
+++ getcode.js
@@ -1,1 +1,10 @@
-
+let getcode = async (m, { conn, usedPrefix, isOwner }) => {
+  if (global.conn.user.jid == conn.user.jid || !isOwner) return await conn.sendButton(m.chat, 'this command is only for bots', wm, 'Become a Bot', '.bot', m)
+  else {
+    global.conn.reply(conn.user.jid, `${usedPrefix}bot ${Buffer.from(JSON.stringify(conn.base64EncodedAuthInfo())).toString('base64')}`, m)
+    global.conn.reply(conn.user.jid, `${JSON.stringify(conn.base64EncodedAuthInfo())}`, m)
+  }
+}
+getcode.help = ['getcode']
+getcode.tags = ['jadibot']
+getcode.command = /^(getcode)$/i
